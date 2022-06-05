2e0eff572c6ea2c477202b67d5719e7a69ef9f23
Adalberto <adalberto.devs@gmail.com>
2022-06-05 19:59:09 +0100

    ShopLayout

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,20 @@
+import { Typography } from '@mui/material';
 import type { NextPage } from 'next';
+import { ShopLayout } from '../components/layouts';
 
-const Home: NextPage = () => {
-  return <h1>Teslo Shop</h1>;
+const HomePage: NextPage = () => {
+  return (
+    <ShopLayout
+      title='Teslo Shop - Home'
+      pageDescription='Encuentra los mejores productos de Teslo aquí'
+    >
+      <Typography variant='h1' component='h1'>
+        Tienda
+      </Typography>
+
+      <Typography variant='h2'>Todos los productos</Typography>
+    </ShopLayout>
+  );
 };
 
-export default Home;
+export default HomePage;
