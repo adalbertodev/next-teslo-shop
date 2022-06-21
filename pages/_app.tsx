f84716fd70fcb44c62578a7cc580e584d90f6527
Adalberto <adalberto.devs@gmail.com>
2022-06-21 18:39:12 +0100

    Paypal payment finished

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,27 +6,34 @@ import { SWRConfig } from 'swr';
 import '../styles/globals.css';
 import { lightTheme } from '../themes';
 import { AuthProvider, CartProvider, UIProvider } from '../context';
+import { PayPalScriptProvider } from '@paypal/react-paypal-js';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <SessionProvider>
-      <SWRConfig
-        value={{
-          fetcher: (resource, init) =>
-            fetch(resource, init).then((res) => res.json())
+      <PayPalScriptProvider
+        options={{
+          'client-id': process.env.NEXT_PUBLIC_PAYPAL_CLIENT_ID || ''
         }}
       >
-        <AuthProvider>
-          <CartProvider>
-            <UIProvider>
-              <ThemeProvider theme={lightTheme}>
-                <CssBaseline />
-                <Component {...pageProps} />
-              </ThemeProvider>
-            </UIProvider>
-          </CartProvider>
-        </AuthProvider>
-      </SWRConfig>
+        <SWRConfig
+          value={{
+            fetcher: (resource, init) =>
+              fetch(resource, init).then((res) => res.json())
+          }}
+        >
+          <AuthProvider>
+            <CartProvider>
+              <UIProvider>
+                <ThemeProvider theme={lightTheme}>
+                  <CssBaseline />
+                  <Component {...pageProps} />
+                </ThemeProvider>
+              </UIProvider>
+            </CartProvider>
+          </AuthProvider>
+        </SWRConfig>
+      </PayPalScriptProvider>
     </SessionProvider>
   );
 }
